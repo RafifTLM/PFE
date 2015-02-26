21217bada20df11b1743e76c10f372046203279f
Rafif <Rafif@Rafif-PC>
2015-02-26 17:14:56 +0100

    change comm

--- test/src/test/testclasse.java
+++ test/src/test/testclasse.java
@@ -4,7 +4,7 @@ public class testclasse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("esskooot :p ");
+		System.out.println("hello word !");
 		
 
 	}
