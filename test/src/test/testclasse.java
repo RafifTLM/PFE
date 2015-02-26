8f6bed09785739c010826d950b8c146dc9adf69d
imsld <ismailinginf@yahoo.fr>
2015-02-27 00:38:19 +0100

    Modif

--- test/src/test/testclasse.java
+++ test/src/test/testclasse.java
@@ -4,9 +4,6 @@ public class testclasse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello word !");
-		System.out.println("hello Sebdou !");
-		System.out.println("hello Tlemcen !");
 	}
 
 }
